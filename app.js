52936b0f53e7511c4d30a437f53cc36afa14bf89
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2023-09-10 23:07:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
-const hourEl = document.getElementById("hour")
-const minuteEl = document.getElementById("minutes")
-const secondEl = document.getElementById("seconds")
-const ampmEl = document.getElementById("ampm")
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minutes");
+const secondEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
 
 function updateClock() {
-    let h = new Date().getHours()
-    let m = new Date().getMinutes()
-    let s = new Date().getSeconds()
-    let ampm = "AM"
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
     if (h > 12) {
-        h = h - 12
-        ampm = "PM"
+        h = h - 12;
+        ampm = "PM";
     }
 
-
     h = h < 10 ? "0" + h : h;
-    m = m < 10 ? "0" + mh : m;
+    m = m < 10 ? "0" + m : m;
     s = s < 10 ? "0" + s : s;
 
     hourEl.innerText = h;
     minuteEl.innerText = m;
     secondEl.innerText = s;
-    ampmEl, (innerText = ampm);
-    setTimeout(() => {
-        updateClock()
-    }, 1000)
+    ampmEl.innerText = ampm; // Fix the assignment here
+    setTimeout(updateClock, 1000); // Use function reference directly
 }
+
 updateClock();
